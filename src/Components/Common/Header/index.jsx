2711192d173d5814efe5b79ad630d27994a25673
Akshin013 <Akshingadimov.@gmail.com>
2026-04-15 16:50:06 +0400

    fix build error

--- src/Components/Common/Header/index.jsx
+++ src/Components/Common/Header/index.jsx
@@ -1,19 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
-import Dealers from '@/app/Dealers/Page'
-import Home from '@/app/page'
-import Banks from '@/app/Banks/Page'
+// import Link from 'next/link'
+// import React from 'react'
+// import Dealers from '../../../app/Dealers/page'
+// import Home from '../../../app/page'
+// import Banks from '../../../app/Banks/page'
 
-const Header = () => {
-  return (
-    <div>
-      <div>ss
-        <Link href="/">Home</Link>
-        <Link href="/Banks">Banks</Link>
-        <Link href="/Dealers">Dilers</Link>
-      </div>
-    </div>
-  )
-}
+// const Header = () => {
+//   return (
+//     <div>
+//       <div>ss
+//         <Link href="/">Home</Link>
+//         <Link href="/Banks">Banks</Link>
+//         <Link href="/Dealers">Dilers</Link>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Header
+// export default Header
--- src/app/dealer/products/page.jsx
+++ src/app/dealer/products/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getDealerProducts } from "@/Services/products.js";
+import { getDealerProducts } from "../../../Services/products.js";
 
 export default function DealerProducts() {
   const [products, setProducts] = useState([]);
